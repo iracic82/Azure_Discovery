0e58f98a63abdc3af4cd0ed2fb3be10c4e897c8c
iracic82 <iracic82@gmail.com>
2025-03-12 14:57:51 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "sp_password" {
-  value     = random_password.sp_password.result
-  sensitive = true  # ✅ Marks as sensitive to avoid showing in logs
+  value     = azuread_service_principal_password.sp_password.value
+  sensitive = true  # ✅ Marks as sensitive to hide in logs
 }
