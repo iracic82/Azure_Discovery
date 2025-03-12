f3f844116316a9849476b4daa763a97ccef62545
iracic82 <iracic82@gmail.com>
2025-03-12 15:15:32 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,12 @@ output "sp_password" {
   value     = azuread_service_principal_password.sp_password.value
   sensitive = true  # ✅ Marks as sensitive to hide in logs
 }
+
+output "client_id" {
+  value = azuread_application.infoblox_app.application_id
+}
+
+output "tenant_id" {
+  value = data.azurerm_client_config.current.tenant_id
+}
+
