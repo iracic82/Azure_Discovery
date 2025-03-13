69caac5cd0a83f9f3f1ebe5ce9cef68e8a835b09
iracic82 <iracic82@gmail.com>
2025-03-13 07:55:14 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,15 +3,13 @@
 #  sensitive = true  # ✅ Marks as sensitive to hide in logs
 #}
 
-#output "client_id" {
-#  value = azuread_application.infoblox_app.application_id
-#}
 output "client_id" {
-  description = "Client ID of the Terraform authenticated Service Principal"
-  value       = data.azurerm_client_config.current.client_id
+  description = "Azure AD Application (Service Principal) Client ID"
+  value       = azuread_application.infoblox_app.application_id
 }
 
 output "tenant_id" {
-  value = data.azurerm_client_config.current.tenant_id
+  description = "Azure Tenant ID"
+  value       = data.azurerm_client_config.current.tenant_id
 }
 
