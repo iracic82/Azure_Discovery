fac89814197bd4ffa065fbf7c9e3e2e2e9b8e2ec
iracic82 <iracic82@gmail.com>
2025-03-13 07:53:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,30 +27,30 @@ provider "azuread" {
 
 data "azurerm_client_config" "current" {}
 
-#resource "azurerm_resource_group" "infoblox_rg" {
-#  name     = "InfobloxOnboardingRG"
-#  location = "East US"
-#}
+resource "azurerm_resource_group" "infoblox_rg" {
+  name     = "InfobloxOnboardingRG"
+  location = "East US"
+}
 
-#resource "azuread_application" "infoblox_app" {
-#  display_name = "InfobloxDiscovery"
-#}
+resource "azuread_application" "infoblox_app" {
+  display_name = "InfobloxDiscovery"
+}
 
-#resource "azuread_service_principal" "infoblox_sp" {
-# application_id = azuread_application.infoblox_app.application_id
-#}
+resource "azuread_service_principal" "infoblox_sp" {
+ application_id = azuread_application.infoblox_app.application_id
+}
 resource "random_password" "sp_password" {
   length           = 32
   special          = true
   override_special = "!@#$%&*()-_=+[]{}<>:?"
 }
 
-#resource "azuread_service_principal_password" "sp_password" {
-#  service_principal_id = azuread_service_principal.infoblox_sp.id
-#  end_date_relative    = "8760h" # 1 year expiry
-#}
+resource "azuread_service_principal_password" "sp_password" {
+  service_principal_id = azuread_service_principal.infoblox_sp.id
+  end_date_relative    = "8760h" # 1 year expiry
+}
 
-/*
+
 resource "azurerm_role_definition" "infoblox_dns_role" {
   name  = "Custom DNS Resolver Full Management"
   scope = "/subscriptions/${var.subscription_id}"
@@ -83,14 +83,14 @@ resource "azurerm_role_definition" "infoblox_dns_role" {
 
   assignable_scopes = ["/subscriptions/${var.subscription_id}"]
 }
-*/
 
-#resource "azurerm_role_assignment" "infoblox_role_assignment" {
-#  scope                = "/subscriptions/${var.subscription_id}"
-#  role_definition_name = "Network Contributor"
-  #principal_id         = azuread_service_principal.infoblox_sp.id
- #  principal_id         = data.azurerm_client_config.current.object_id
-#}
+
+resource "azurerm_role_assignment" "infoblox_role_assignment" {
+  scope                = "/subscriptions/${var.subscription_id}"
+  role_definition_name = azurerm_role_definition.infoblox_dns_role.name
+  principal_id         = azuread_service_principal.infoblox_sp.id
+ 
+}
 
 
 
