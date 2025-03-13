37d548565a3d00eef1c5f1058bba23bbbb3c7c7a
iracic82 <iracic82@gmail.com>
2025-03-13 06:41:24 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,12 @@ output "sp_password" {
   sensitive = true  # ✅ Marks as sensitive to hide in logs
 }
 
+#output "client_id" {
+#  value = azuread_application.infoblox_app.application_id
+#}
 output "client_id" {
-  value = azuread_application.infoblox_app.application_id
+  description = "Client ID of the Terraform authenticated Service Principal"
+  value       = data.azurerm_client_config.current.client_id
 }
 
 output "tenant_id" {
