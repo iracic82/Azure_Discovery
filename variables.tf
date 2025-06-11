576356e7549a8266abf7a293fe8537f9b6deee8e
iracic82 <iracic82@gmail.com>
2025-06-11 16:01:43 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,1 @@
-variable "subscription_id" {
-  description = "Azure Subscription ID where permissions will be applied"
-  type        = string
-}
 
-variable "infoblox_app_id" {
-  description = "Infoblox Application (Client) ID"
-  type        = string
-}
